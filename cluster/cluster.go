66e9beaebd94a0e5a8f9530ee0e309ba8d879c97
Quinn Slack <qslack@qslack.com>
2014-06-19 08:07:43 -0700

    use port 80

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -15,17 +15,17 @@ import (
 type Client struct {
 	dir *Directory
 
-	c *etcd.Client
+	etcdClient *etcd.Client
 
 	// HTTP client used to communicate with the vcsstore API.
 	httpClient *http.Client
 }
 
-func NewClient(httpClient *http.Client) *Client {
-	c := etcd.NewClient([]string{"http://localhost:4001"})
+func NewClient(etcdClient *etcd.Client, httpClient *http.Client) *Client {
 	return &Client{
-		dir: &Directory{c},
-		c:   c,
+		dir:        &Directory{etcdClient},
+		httpClient: httpClient,
+		etcdClient: etcdClient,
 	}
 }
 
@@ -35,14 +35,14 @@ func (c *Client) Repository(vcsType string, cloneURL *url.URL) (vcs.Repository, 
 	if err == ErrNoOwner {
 		// This repository doesn't exist yet. Pick a machine at random from
 		// the cluster to designate the owner.
-		ms := c.c.GetCluster()
+		ms := c.etcdClient.GetCluster()
 		log.Printf("### etcd cluster is: %v", ms)
 		if len(ms) == 1 {
 			owner = ms[0]
 		} else {
 			owner = ms[rand.Intn(len(ms)-1)]
 		}
-		owner = strings.Replace(owner, ":4001", ":3000", -1)
+		owner = strings.Replace(owner, ":4001", ":80", -1)
 
 		log.Printf("### etcd vcsclient: setting owner %q for %s %s", owner, vcsType, cloneURL)
 		err := c.dir.SetOwner(key, owner)
