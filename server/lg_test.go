d05bd5d53ca282a1e7e360d6d454388e7ed4c49d
Quinn Slack <qslack@qslack.com>
2014-10-21 15:36:41 -0700

    Integration tests for cloning and updating

--- server/lg_test.go
+++ server/lg_test.go
@@ -13,6 +13,8 @@ import (
 
 	"github.com/sourcegraph/go-vcs/vcs"
 	_ "github.com/sourcegraph/go-vcs/vcs/git"
+	_ "github.com/sourcegraph/go-vcs/vcs/hg"
+	_ "github.com/sourcegraph/go-vcs/vcs/hgcmd"
 	"github.com/sourcegraph/vcsstore"
 	"github.com/sourcegraph/vcsstore/vcsclient"
 )
@@ -22,11 +24,45 @@ var (
 	privateRepo = flag.String("privrepo", "ssh://git@github.com/sourcegraph/private-repo.git", "a private, SSH-accessible repo to test cloning")
 )
 
-func TestClone_lg(t *testing.T) {
+func TestCloneGitHTTPS_lg(t *testing.T) {
+	t.Parallel()
+	testClone_lg(t, "git", "https://github.com/sgtest/empty-repo.git", vcs.RemoteOpts{})
+}
+
+func TestCloneGitGit_lg(t *testing.T) {
+	t.Parallel()
+	testClone_lg(t, "git", "git://github.com/sgtest/empty-repo.git", vcs.RemoteOpts{})
+}
+
+func TestCloneGitSSH_lg(t *testing.T) {
+	t.Parallel()
+	if *sshKeyFile == "" {
+		t.Skip("no ssh key specified")
+	}
+
+	var opt vcs.RemoteOpts
+	if *sshKeyFile != "" {
+		key, err := ioutil.ReadFile(*sshKeyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		opt.SSH = &vcs.SSHConfig{PrivateKey: key}
+	}
+
+	testClone_lg(t, "git", *privateRepo, opt)
+}
+
+func TestCloneHgHTTPS_lg(t *testing.T) {
+	t.Parallel()
+	testClone_lg(t, "hg", "https://bitbucket.org/sqs/go-vcs-hgtest", vcs.RemoteOpts{})
+}
+
+func testClone_lg(t *testing.T, vcsType, repoURLStr string, opt vcs.RemoteOpts) {
 	storageDir, err := ioutil.TempDir("", "vcsstore-test")
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer os.RemoveAll(storageDir)
 
 	conf := &vcsstore.Config{
 		StorageDir: storageDir,
@@ -46,22 +82,13 @@ func TestClone_lg(t *testing.T) {
 		t.Fatal(err)
 	}
 	c := vcsclient.New(baseURL, nil)
-	repoURL, err := url.Parse(*privateRepo)
+	repoURL, err := url.Parse(repoURLStr)
 	if err != nil {
 		t.Fatal(err)
 	}
-	repo, err := c.Repository("git", repoURL)
+	repo, err := c.Repository(vcsType, repoURL)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	var opt vcs.RemoteOpts
-	if *sshKeyFile != "" {
-		key, err := ioutil.ReadFile(*sshKeyFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		opt.SSH = &vcs.SSHConfig{PrivateKey: key}
 	}
 
 	if repo, ok := repo.(vcsclient.RepositoryCloneUpdater); ok {
