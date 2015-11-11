0c93a215d180f738b72a1c581917231aceff3c46
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2015-11-11 16:29:45 -0700

    vcsclient: add tests for invalid (negative, OOB, inverted) byte/line ranges to ComputeFileRange

--- vcsclient/range_test.go
+++ vcsclient/range_test.go
@@ -69,3 +69,65 @@ func TestComputeFileRange(t *testing.T) {
 		_ = test.data[got.StartByte:got.EndByte]
 	}
 }
+
+func TestComputeBadFileRange(t *testing.T) {
+	tests := map[string]struct {
+		data []byte
+		opt  GetFileOptions
+		want string
+	}{
+		// Byte ranges
+		"negative start byte": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartByte: -1, EndByte: 3}},
+			want: "start byte -1 out of bounds (4 bytes total)",
+		},
+		"OOB end byte": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartByte: 0, EndByte: 5}},
+			want: "end byte 5 out of bounds (4 bytes total)",
+		},
+		"inverse valid bytes": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartByte: 3, EndByte: 0}},
+			want: "start byte (3) cannot be greater than end byte (0) (4 bytes total)",
+		},
+		"inverse 2 lines, byte range": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartByte: 3, EndByte: 2}},
+			want: "start byte (3) cannot be greater than end byte (2) (4 bytes total)",
+		},
+		"inverse 2 lines, byte range, full lines": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartByte: 3, EndByte: 2}, FullLines: true},
+			want: "start byte (3) cannot be greater than end byte (2) (4 bytes total)",
+		},
+
+		// Line ranges
+		"negative start line": {
+			data: []byte("a\nb"),
+			opt:  GetFileOptions{FileRange: FileRange{StartLine: -1, EndLine: 1}},
+			want: "start line -1 out of bounds (2 lines total)",
+		},
+		"inverse 2 lines, no trailing newline": {
+			data: []byte("a\nb"),
+			opt:  GetFileOptions{FileRange: FileRange{StartLine: 2, EndLine: 1}},
+			want: "start line (2) cannot be greater than end line (1) (2 lines total)",
+		},
+		"inverse 2 lines, trailing newline": {
+			data: []byte("a\nb\n"),
+			opt:  GetFileOptions{FileRange: FileRange{StartLine: 2, EndLine: 1}},
+			want: "start line (2) cannot be greater than end line (1) (2 lines total)",
+		},
+	}
+	for label, test := range tests {
+		got, _, err := ComputeFileRange(test.data, test.opt)
+		if err == nil || err.Error() != test.want {
+			t.Errorf("%s: got %q, want %q", label, err, test.want)
+			continue
+		}
+		if got != nil {
+			t.Errorf("%s: expected nil result", label)
+		}
+	}
+}
